f541921a9d42bc2f5153115afff0acefa3416e93
escrib <sebascraton@gmail.com.com>
2024-05-16 23:27:27 -0300

    botones-del-menu-ya-no-se-borran-en-modo-desktop

--- js/onScroll_btn-top_navBar.js
+++ js/onScroll_btn-top_navBar.js
@@ -3,11 +3,6 @@ let goTop = () => {
     const button = document.getElementById("goTop");
     const header_shadow = document.getElementById("headerId");
     let nav_menu_items = document.querySelectorAll(".nav_menu_items");
-    let nav_menu_items1 = document.querySelector(".nav_menu_items");
-    let menu_input = document.querySelector("#menu-input");
-    let menu_burguer = document.querySelector(".menu-burguer");
-    let menu_equ = document.querySelector(".menu-equ");
-    let nav_menu = document.querySelector(".nav_menu");
 
     window.onscroll = () => {
         button.classList[
@@ -19,16 +14,13 @@ let goTop = () => {
         } else {
             header_shadow.classList.add("header_Top_Max");
             document.querySelector('.title_h1').style.fontSize = "5rem";
-
         }
-
 
         if (document.documentElement.scrollTop > 1) {
             nav_menu_items[0].style.top = "30px";
         } else {
             nav_menu_items[0].style.top = "50px";
         }
-
     }
 
     button.onclick = () => {
@@ -37,8 +29,7 @@ let goTop = () => {
         })
     }
 
-
-    const menus = () => {
+    /*const menuBpMobiles = () => {
 
         if (window.innerWidth < 770) {
 
@@ -79,8 +70,65 @@ let goTop = () => {
 
         }
     }
-    menus();
-
+    menuBpMobiles();*/
 };
+
+const menuBpMobiles = () => {
+    let nav_menu_items = document.querySelectorAll(".nav_menu_items");
+    let menu_input = document.querySelector("#menu-input");
+    let menu_burguer = document.querySelector(".menu-burguer");
+    let menu_equ = document.querySelector(".menu-equ");
+    let nav_menu = document.querySelector(".nav_menu");
+
+    if (window.innerWidth < 770) {
+
+        let menuShow = () => {
+            menu_burguer.addEventListener("click", () => {
+                menu_burguer.style.visibility = "hidden";
+                menu_equ.style.visibility = "visible";
+                nav_menu.style.visibility = "visible";
+            });
+        }
+        menuShow();
+
+        let menuClickedNoShow = () => {
+            nav_menu_items.forEach(item => {
+                item.addEventListener("click", () => {
+                    menu_equ.style.visibility = "hidden";
+                    menu_burguer.style.visibility = "visible";
+                    nav_menu.style.visibility = "hidden";
+                    if (menu_input.checked === true) {
+                        menu_input.checked = false;
+                    }
+                });
+            });
+        }
+        menuClickedNoShow();
+
+        let equShow = () => {
+            menu_equ.addEventListener("click", () => {
+                menu_burguer.style.visibility = "visible";
+            })
+        }
+        equShow();
+    }
+}
+
+const menuBpDesktop = () => {
+    let nav_menu = document.querySelector(".nav_menu");
+    let nav_menu_items1 = document.querySelector(".nav_menu_items");
+
+    if (window.innerWidth >= 770) {
+        nav_menu.style.visibility = "visible";
+        nav_menu_items1.style.display = "flex";
+        nav_menu_items1.style.visibility = "visible";
+    }
+
+}
+
+window.addEventListener("resize", () => {
+    menuBpDesktop();
+    menuBpMobiles();
+});
 
 goTop();
