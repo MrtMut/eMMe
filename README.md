a4e6c364f44d8e7b399fcd443a51d25f13abf886
escrib <sebascraton@gmail.com.com>
2024-05-29 01:46:32 -0300

    finalizing cases

--- README.md
+++
@@ -1,1 +0,0 @@
-Esta pagina es sobre programadores
--- js/year.js
+++
@@ -1,4 +0,0 @@
-// Display the current year on the footer
-const date = new Date();
-const year = date.getFullYear();
-document.getElementById("year").innerText = `${year}`;
