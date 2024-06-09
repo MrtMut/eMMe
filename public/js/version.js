20555f559bfff4f619dc19be4d3e4a36d48258eb
escrib <sebascraton@gmail.com.com>
2024-06-09 00:44:28 -0300

    admin

---
+++ public/js/version.js
@@ -0,0 +1,4 @@
+// This file is used to display the version of the application on the home page.
+let version_app;
+version_app = "1.0.0";
+document.getElementById("version").innerText = `v${version_app}`;
