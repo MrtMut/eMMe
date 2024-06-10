2e8525bf669d4063b823f6477c4c448519fb5aac
escrib <sebascraton@gmail.com.com>
2024-06-09 23:35:30 -0300

    admin

--- public/js/admin_footer.js
+++ public/js/admin_footer.js
@@ -2,10 +2,13 @@
 let admin_footer = document.getElementById("admin_footer");
 admin_footer.innerHTML = `<div class="footer_section_links">
         <ul>
-            <li class="admin_links"><a href="./login.html">Login</a></li>
-            <li class="admin_links"><a href="./register.html">Register</a></li>
-            <li class="admin_links"><a href="./edit.html">Edit</a></li>
-            <li class="admin_links"><a href="./create.html">Create</a></li>
+            <li class="admin_links"><a href="../admin/login.html">Login</a></li>
+            <li class="admin_links"><a href="../admin/register.html">Register</a></li>
+            <li class="admin_links"><a href="../admin/proyectos.html">Proyectos</a></li>
+            <li class="admin_links"><a href="../admin/create.html">Create</a></li>
+            <li class="admin_links"><a href="../admin/edit.html">Edit</a></li>
+            <li class="admin_links"><a href="../admin/delete.html">Delete</a></li>
+
         </ul>
     </div>
     <p class="copy"> &copy; Copyright {eMMe} <span id="year"></span> - <span id="version"></span> </p>`;
