3ccc5834bbf7dd05b204817b55f33e8537addb52
escrib <sebascraton@gmail.com.com>
2024-06-11 00:48:51 -0300

    admin

--- js/admin_footer.js
+++ js/admin_footer.js
@@ -1,10 +1,12 @@
 
 let admin_footer = document.getElementById("admin_footer");
 admin_footer.innerHTML = `<div class="footer_section_links">
-        <ul>
+        <ul class="footer_links_left">
             <li class="admin_links"><a href="../admin/login.html">Login</a></li>
             <li class="admin_links"><a href="../admin/register.html">Register</a></li>
             <li class="admin_links"><a href="../admin/proyectos.html">Proyectos</a></li>
+        </ul>
+        <ul class="footer_links_right">
             <li class="admin_links"><a href="../admin/create.html">Create</a></li>
             <li class="admin_links"><a href="../admin/edit.html">Edit</a></li>
             <li class="admin_links"><a href="../admin/delete.html">Delete</a></li>
