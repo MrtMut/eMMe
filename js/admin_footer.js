e101e3f8c76963fb823e32de3c2920afc9216b91
escribroid <sebascraton@gmail.com>
2024-06-29 00:58:10 -0300

    Initial commit

--- js/admin_footer.js
+++ js/admin_footer.js
@@ -4,7 +4,6 @@ admin_footer.innerHTML = `<div class="footer_section_links">
         <ul class="footer_links_right">
             <li class="admin_links"><a href="../admin/projects_admin.html">Proyectos</a></li>
             <li class="admin_links"><a href="../admin/create.html">Create</a></li>
-            <li class="admin_links"><a href="../admin/edit.html">Edit</a></li>
         </ul>
     </div>
     <p class="copy"> &copy; Copyright {eMMe} <span id="year"></span> - <span id="version"></span> </p>`;
--- js/admin_nav_menu_admin.js
+++ js/admin_nav_menu_admin.js
@@ -4,8 +4,6 @@ nav_menu_admin.innerHTML = `<ul class="nav_menu_items_admin">
     <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin log_reg_color" href="./register.html">Register</a></li>
     <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin"  href="./projects_admin.html">Proyectos </a></li>
     <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin" href="./create.html">Create</a></li>
-    <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin" href="./edit.html">Edit</a></li>
-    <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin" href="./delete.html">Delete</a></li>
     <li class=" nav_menu_item_admin"><a class="nav_menu_link nav_menu_link_admin log_reg_color" href="./logout.html">Salir</a></li>
 
                             </ul>`;
